8300343770f8f14683c6a30b0d0a49e849bdb776
Julien Usson <usson.julien@gmail.com>
2018-11-13 19:12:23 +0100

    :sparkles: better tp tests

--- src/scenes/exercices/Testing.js
+++ src/scenes/exercices/Testing.js
@@ -6,25 +6,21 @@ export default () => (
   <Markdown>{`
 # TP n°6 : Les tests !
 
-Les tests se trouvent dans un répertoire \`__tests__\` sont exécutés via la commande \`npm run test\` (ou \`npm run isima-test\`).
+Les tests se trouvent dans les répertoires \`__tests__\`. Ils s'exécutent via la commande \`npm run test\` (ou \`npm run isima-test\`).
 
-Des exemples sont présents dans \`src/utils/__tests__/\` et dans \`src/__tests__/\`.
+Des exemples de test sont présents dans \`src/utils/__tests__/\` et dans \`src/__tests__/\`.
 
 ## Tests unitaire
 
-Mettre en place des tests unitaires sur toutes les fonctions de service.
+Mettre en place des tests unitaires sur les fonctions de service.
 
 Par exemple : Import pizzas, Export pizzas, login, logout, etc...
 
-Il faut tester chaque fonction qu'on utilise et qui pourrait poser des soucis plus tard lors de la vie du projet.
-
 ## Tests d'intégration
 
-Mettre en place des tests d'intégration sur toutes les routes.
+Mettre en place des tests d'intégration.
 
 Par exemple : GET "/", GET "/login", etc
-
-Le but est de tester que les pages s'affichent comme elles se doient.
 
 ## Autres ressources
 
